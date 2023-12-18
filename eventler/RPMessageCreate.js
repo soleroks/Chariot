4a31af3b93180eb6841bc0098ab8f7f43b8f0961
mirage <82513212+00xmirage@users.noreply.github.com>
2023-12-19 02:11:02 +0300

    All events
    I didn't include the command handler and necessary peripherals to run the bot since I don't want to expose my spagetti code. 👌

---
+++ eventler/RPMessageCreate.js
@@ -0,0 +1,32 @@
+
+const Discord = require('discord.js')
+const uyeSema = require('../şemalar/PoAUser')
+const ayarlar = require('../ayarlar.json')
+const moment = require('moment')
+moment.locale('tr')
+module.exports = {
+    name : Discord.Events.MessageCreate,
+    on: true,
+    async execute(message) {
+        if(message.content.startsWith('//')) {
+          setInterval(() => {
+            message.delete()    
+          }, 5000);
+        }
+
+  
+        if(message.author.bot) return
+        
+        if(!ayarlar.rpKategoriler.includes(message.channel.parent.id)) return
+let string = message.content.replace(/ /g, "")
+let RPPuanFinal = string.length / 4
+
+await uyeSema.findOneAndUpdate({"kullanici.userID" : message.author.id}, { $inc: {"kullaniciRP.RPPuan.genelPuan": +RPPuanFinal}})
+await uyeSema.findOneAndUpdate({"kullanici.userID" : message.author.id}, { $inc: {"kullaniciRP.RPPuan.haftalikPuan": +RPPuanFinal}})
+await uyeSema.findOneAndUpdate({"kullanici.userID": message.author.id}, {$set: {"kullaniciRP.RPBilgi.sonTarih": moment().unix()}})
+await uyeSema.findOneAndUpdate({"kullanici.userID": message.author.id}, {$set: {"kullaniciRP.RPBilgi.sonKanalURL" : message.url}})
+await uyeSema.findOneAndUpdate({"kullanici.userID": message.author.id}, {$set: {"kullaniciRP.RPBilgi.sonKanal" : message.channel.id}})
+
+
+      }
+    }
