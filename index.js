4b66bb08186917c6975e364be9dafaacd07ab145
soleroks <emirmelikakts@gmail.com>
2024-08-11 03:49:38 +0300

    ufak değişiklikler yapıldı.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// CHARIOT V14 BY SOLEROKS
-
-// TANOMLAR
-
+/**
+ * Chariot 1.4 (Venice)
+ * 
+ * - Chariot 1'in üzerine ufak çaplı hata düzeltmeleri ve fonksiyon geliştirmeleri içeren, Chariot 2 uyumlu bir ara sürümdür.
+ * 
+ * Yapılan değişiklikler:
+ *	 - Gereksiz event ve komutlar silindi.
+	 - ChariotAPI entegrasyonu eklendi.
+	 - Chariot kullanıcı direktifleri desteği eklendi. 
+	 - Sağ tık komutları düzenlendi.
+	 - Gereksiz kütüphane dosyaları kaldırıldı.
+	 - Chariot Time eklendi.
+	 - 
+ */
 const Discord = require('discord.js')
 const mongoose = require('mongoose')
-let fonksiyon = require('./utils/fonksiyon.js')
 const moment = require('moment')
 moment.locale('tr')
 const allIntents = new Discord.IntentsBitField(3276799)
@@ -12,19 +21,10 @@ const client = new Discord.Client({partials: [Discord.Partials.Message],intents:
 const ayarlar = require('./ayarlar.json')
 const path = require('path')
 const fs = require('fs')
-const { error } = require('console')
-// BOOT
-client.on('ready', on => {
-    client.user.setPresence({status: "idle"})
-	
-	let bilgi = ayarlar.dbSelector
-	let bilgi2 = ""
-	if(bilgi === "dev") bilgi2 = "bakım"
-	if(bilgi === "live") bilgi2 = "sunucu"
 
-})
-// APİ GİRİŞ
 client.login(ayarlar.token.dev)
+
+
 
 // EVENT HANDLER
 const eventsPath = path.join(__dirname, 'eventler');
@@ -61,31 +61,74 @@ for (const file of commandFiles) {
 		console.log(`[UYARI] ${filePath} yolundaki komut "data" veya "execute" argümanı içermiyor. ℹ`);
 	}
 }
-// FONKSİYONLAR
-client.soLCode = fonksiyon.SOLCodeOlustur
-client.kanalGetir = fonksiyon.kanalGetir
-client.uyeGetir = fonksiyon.uyeGetir
-client.bakiyeTransfer = fonksiyon.bakiyeEkle
-client.mongoose = require('mongoose')
-client.itemEkle = fonksiyon.esyaEkle
-client.itemCikar = fonksiyon.esyaSil
-client.dbBaglan = require('./utils/dbConnect.js').dbSelector
-// BAKİYE DÜZENLEYİCİ INDEX
 
-/** 
+
+
 client.on(Discord.Events.InteractionCreate, async interaction => { 
-	if (!interaction.isModalSubmit()) return;
-	
+    if (!interaction.isModalSubmit()) return;
+    
+    if(interaction.customId === "odemeform") {
 
-	const bakiye = interaction.fields.getTextInputValue('uyeBakiyegirisi');
-	const ID = interaction.fields.getTextInputValue('uyeIDgirisi');
-	console.log(bakiye, ID);
-	if(!interaction.guild.members.cache.get(ID)) {
-	await interaction.reply({ephemeral:true ,content: "Bu ID'ye sahip bir üye sunucuda olmadığı için işleminize devam edilemedi. Lütfen düzenlemek istediğiniz üyenin ID'sini kontrol edip tekrar deneyiniz." });
-
-
-	}
+    const bakiye = interaction.fields.getTextInputValue('uyeBakiyegirisi');
+    const Veri = await YetkiliSema.findOne({userID: interaction.user.id})
+    console.log(Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID, bakiye);
+    if(!interaction.guild.members.cache.get(Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID)) {
+    await interaction.reply({ephemeral:true ,content: "Belirtilen üye bulunamadı." });
 
 
-});
-*/
+    }
+    else if(!await uyeSema.findOne({"kullanici.userID":Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID })) {
+        await interaction.reply({ephemeral:true ,content: "Bu üyeye ait veri yok." });
+
+    }
+    else if(interaction.guild.members.cache.get(Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID)) {
+        await uyeSema.findOneAndUpdate({"kullanici.userID": Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID}, {
+            $inc: {
+                "kullaniciJC.Bakiye" : bakiye
+            }
+        })
+
+
+        let durum = "ekleme"
+
+        if(bakiye.startsWith('+')) durum = "eklendi"
+        if(bakiye.startsWith('-')) durum = "çıkartma"
+
+        await uyeSema.findOneAndUpdate({"kullanici.userID": Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID}, {
+            $set: {
+                "kullaniciJC.BakiyeSonIslem.sonIslemTur" : `Düzeltme - ${durum}`
+            }
+        })
+
+        await uyeSema.findOneAndUpdate({"kullanici.userID": Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID}, {
+            $set: {
+                "kullaniciJC.BakiyeSonIslem.sonIslemZaman" : moment().unix()
+            }
+        })
+
+        await uyeSema.findOneAndUpdate({"kullanici.userID": Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID}, {
+            $set: {
+                "kullaniciJC.BakiyeSonIslem.sonIslemKısı" : "Sistem"
+            }
+        })
+        
+        await uyeSema.findOneAndUpdate({"kullanici.userID": Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID}, {
+            $set: {
+                "kullaniciJC.BakiyeSonIslem.sonIslemMiktar" : bakiye
+            }
+        })
+        
+        
+
+
+    
+        interaction.reply({embeds: [
+            new Discord.EmbedBuilder()
+            .setDescription(`<@${Veri.YetkiliBakiyeDuzenleyici.bakiyeTargetID}> kişsinin bakiyesi başarıyla düzenlenmiştir.`)
+            .setColor('DarkOrange')
+            .setFooter({text: "Path of Ascension Bakiye Düzenleyici"})
+
+        ],ephemeral: true})
+    }
+    }
+})
